ab0909133c0dad8793fe394ad97ef828ad5d93b5
dana coller <danacoler@gmail.com>
2011-08-01 14:57:24 +0300

    added PathParam annotaion support

--- clever_dropbox_server/src/main/scala/CleverDropboxServer.scala
+++ clever_dropbox_server/src/main/scala/CleverDropboxServer.scala
@@ -2,16 +2,13 @@ package cleverDropboxServer
 
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 import javax.servlet.ServletException
- 
 import java.io.IOException
- 
 import org.eclipse.jetty.server.{Server, Request}
 import org.eclipse.jetty.server.handler.AbstractHandler
 import org.eclipse.jetty.server.nio.SelectChannelConnector
 import org.eclipse.jetty.servlet.{ServletHolder, ServletContextHandler}
 import com.sun.jersey.spi.container.servlet.ServletContainer
-
-import javax.ws.rs.{GET, Produces, Path}
+import javax.ws.rs.{GET, Produces, Path, PathParam}
 
 
 object WebRunner extends App{
@@ -39,5 +36,16 @@ class TestResource {
     def hello() = {
         "<html><title>GilaDana Inc.</title>" +
         """<body><img src="http://www.h-online.com/imgs/43/5/5/4/4/8/6/a484cda3f9839fe6.png"></img> RULEZZZZZZZ!!!</body></html>"""
+    }
+}
+
+
+@Path("/user/{username}")
+class SomethingElse {
+    @GET
+    //@Produces("text/html")
+    def goodbye(@PathParam("username") user:String):String = {
+        "<html><title>GilaDana Inc.</title>" +
+        "<body>" + user + " ROCKSSSSSSSS!!!</body></html>"
     }
 }
