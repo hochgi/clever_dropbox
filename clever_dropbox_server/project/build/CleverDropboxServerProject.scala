f108d9efb65b9dc1c1a422cd66d773682041e28f
Gilad Hoch <giladhoch@yahoo.com>
2011-08-01 16:42:59 +0300

    added the apache.commons dependencies

--- clever_dropbox_server/project/build/CleverDropboxServerProject.scala
+++ clever_dropbox_server/project/build/CleverDropboxServerProject.scala
@@ -4,6 +4,8 @@ import de.element34.sbteclipsify._
 class CleverDropboxServerProject(info: ProjectInfo) extends DefaultWebProject(info) with Eclipsify {
 
     val javanetDeps = "javanetDeps" at "http://download.java.net/maven/2/"
+    val apacheSnap = "apache.snapshots" at "http://people.apache.org/repo/m2-snapshot-repository"
+    val central = "central" at "http://repo1.maven.org/maven2"
 
 //some might be needed repositories...
 //    val snapshots = "jersey-server Snapshots" at "http://download.java.net/maven/2/"
@@ -13,14 +15,19 @@ class CleverDropboxServerProject(info: ProjectInfo) extends DefaultWebProject(in
 	val jersey_core = "com.sun.jersey" % "jersey-core" % "1.8"
     val jersey_json = "com.sun.jersey" % "jersey-json" % "1.8"
     val jsr311 = "javax.ws.rs" % "jsr311-api" % "1.1.1"
+
 //    val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.24" % "test"  // jetty is only need for testing (copied from: http://code.google.com/p/simple-build-tool/wiki/WebApplications)
     val jettyServer = "org.eclipse.jetty" % "jetty-server" % "8.0.0.M0"
     val jettyServlet = "org.eclipse.jetty" % "jetty-servlet" % "8.0.0.M0"
 
+    val apacheCommons = "commons-io" % "commons-io" % "1.3.2"
+    val servlet_api = "javax.servlet" % "servlet-api" % "2.5"
+    val portlet_api= "portlet-api" % "portlet-api" % "1.0"
+
 //some might be needed dependencies...
 
 //    val jersey-multipart = "com.sun.jersey.contribs" % "jersey-multipart" % "1.8"
-//    val servlet-api = "javax.servlet" % "servlet-api" % "2.5"
+//
 //    val jetty-plugin = "org.mortbay.jetty" % "jetty-plugin" % "6.1.24"
 //    val = "" % "" % ""
 }
